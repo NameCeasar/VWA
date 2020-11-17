6f49f120d9613b5b9a00ce0969da23708a3f2afc
NameCeasar <namyce19@gmail.com>
2020-11-17 08:55:45 +0100

    version of 17.11

--- SRC/Main.cpp
+++ SRC/Main.cpp
@@ -2,21 +2,11 @@
 
 int main() {
   VWAPI::init();
-  //UART u;
-  GPIO a(15, GPIO::OUTPUT);
-  GPIO b(14, GPIO::OUTPUT);
-  GPIO c(2, GPIO::OUTPUT);
-  GPIO d(3, GPIO::OUTPUT);
+  LED l;
   while(true) {
-    a.setLevel(true);
-    b.setLevel(true);
-    c.setLevel(true);
-    d.setLevel(true);
+    l.set(LED::GREEN);
     wait_ms(100);
-    a.setLevel(false);
-    b.setLevel(false);
-    c.setLevel(false);
-    d.setLevel(false);
+    l.set(LED::RED);
     wait_ms(100);
   }
 }
